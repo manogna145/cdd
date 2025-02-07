47f44cbdcdf51c8cb3960662f6a5c4d694e36660
manogna145 <manogna.krk@gmail.com>
2025-02-07 10:18:19 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,10 @@
+#this program adds two numbers
+
+a=1.5
+b=6.3
+
+sum=a+b
+print('the sum of {0} and {}[D1} is {2} ' format(a,b,sum)
+
+[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[C[C[C[C[C[[B[B[B[B[B[B[B[F[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D.f[C[A[A[A[A[B[B[B[B[Dormat(a,b,sum)
+
